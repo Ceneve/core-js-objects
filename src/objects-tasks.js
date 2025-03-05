fdf59df70fa8b6e138fd2adb6caaa271743d9634
Ceneve <boriskozlow@gmail.com>
2025-03-05 15:06:40 +0300

    feat: solve sellTickets, Rectangle, fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -145,8 +145,17 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let i = 0;
+  i += queue.shift();
+  while (queue.length > 0) {
+    i += 25;
+    if (i - queue[0] < 0) {
+      return false;
+    }
+    queue.shift();
+  }
+  return true;
 }
 
 /**
@@ -162,8 +171,14 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return this.width * this.height;
+    },
+  };
 }
 
 /**
@@ -191,8 +206,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  Object.setPrototypeOf(obj, proto);
+  return obj;
 }
 
 /**
